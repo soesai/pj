c62f7253cff0184b726fedca10ef8200797bc0b5
soesai <44113425+soesai@users.noreply.github.com>
2022-10-20 14:05:38 +0630

    Update remote.js
    write ready function

--- remote.js
+++ remote.js
@@ -192,13 +192,22 @@ try{
     })
 }catch (error) {
     console.log("Pure Js Mode")
-    document.onreadystatechange = function() {
-        if (document.readyState == "complete" || document.readyState === 'interactive') {
-           console.log("Document is ready")
-           checkPageDecision()
-        }
-     }
+    ready(function(){
+        console.log("Document is Ready")
+        checkPageDecision()
+    })
 }
+
+function ready(callback) {
+    // in case the document is already rendered
+    if (document.readyState != 'loading') callback();
+    // modern browsers
+    else if (document.addEventListener) document.addEventListener('DOMContentLoaded', callback);
+    // IE <= 8
+    else document.attachEvent('onreadystatechange', function() {
+       if (document.readyState == 'complete') callback();
+    });
+ }
 
 
 function checkPageDecision(){
